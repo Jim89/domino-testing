35c5ab8f079c8ba0da0e18ae71516b918a5ba755
Jim Leach <jimmy22theave@gmail.com>
2017-01-07 11:06:25 +0000

    Add training code train file

--- train.py
+++ train.py
@@ -5,3 +5,31 @@ Created on Sat Jan  7 11:05:36 2017
 @author: jim
 """
 
+#==============================================================================
+# Create simple classifier for gender based purely on name
+# Example as shown in http://www.nltk.org/book/ch06.html
+#==============================================================================
+
+# First up, define a simple feature extractor based on the last letter of the name
+# (shown to be gender-informative)
+def gender_feature(word):
+    return {'last_letter': word[-1]}
+    
+# Now pull through the sample of names from nltk to prepare list of example names
+from nltk.corpus import names 
+
+# Now label the names
+labelled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
+   
+# Then randomly shuffle them up
+import random
+random.shuffle(labelled_names)    
+
+# Then pull out the simple feature (last letter) for each name, along with the specified gender
+featuresets = [(gender_feature(n), gender) for (n, gender) in labelled_names]  
+
+# Then split into test and train
+train, test = featuresets[500:], featuresets[:500]  
+
+# Create a dead-simple Naive-Bayes classifier from the train set
+classifier = nltk.NaiveBayesClassifier.train(train)
